ec26ca528d7c47b14971285df5205ca4c31c9081
dramp <dramppet@abv.bg>
2023-11-30 22:53:56 +0200

    Stack

--- Liner_DSA_SoftUni/Skeleton-Lab/src/main/java/implementations/Queue.java
+++ Liner_DSA_SoftUni/Skeleton-Lab/src/main/java/implementations/Queue.java
@@ -1,10 +1,20 @@
 package implementations;
 
+import com.company.dsa.Node;
 import interfaces.AbstractQueue;
 
 import java.util.Iterator;
 
 public class Queue<E> implements AbstractQueue<E> {
+
+    private static class Node<E>{
+        private E element;
+        private Node<E> next;
+
+        public Node(E value){
+            this.element = value;
+        }
+    }
 
     @Override
     public void offer(E element) {
--- Liner_DSA_SoftUni/Skeleton-Lab/src/main/java/implementations/Stack.java
+++ Liner_DSA_SoftUni/Skeleton-Lab/src/main/java/implementations/Stack.java
@@ -1,38 +1,86 @@
 package implementations;
 
+import com.company.dsa.Node;
 import interfaces.AbstractStack;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class Stack<E> implements AbstractStack<E> {
 
+    private Node<E> top;
+    private int size;
+
+    private static class Node<E> {
+        private E value;
+        private Node<E> next;
+
+        public Node(E element) {
+            this.value = element;
+        }
+    }
+
+    public Stack() {
+        this.top = null;
+        this.size = 0;
+    }
+
     @Override
     public void push(E element) {
+        Node<E> pushElement = new Node<>(element);
 
+        pushElement.next = this.top;
+        this.top = pushElement;
+        this.size++;
     }
 
     @Override
     public E pop() {
-        return null;
+        ensureNotEmpty();
+        Node<E> tmp = this.top;
+        this.size--;
+        this.top = tmp.next;
+        return tmp.value;
     }
 
     @Override
     public E peek() {
-        return null;
+        ensureNotEmpty();
+        return this.top.value;
     }
 
     @Override
     public int size() {
-        return 0;
+        return this.size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return this.size == 0;
     }
 
     @Override
     public Iterator<E> iterator() {
-        return null;
+        return new Iterator<E>() {
+            private Node<E> current = top;
+
+            @Override
+            public boolean hasNext() {
+                return this.current != null;
+            }
+
+            @Override
+            public E next() {
+                E value = this.current.value;
+                return this.current = this.current.next;
+                return value;
+            }
+        };
+    }
+
+    private void ensureNotEmpty() {
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
     }
 }
