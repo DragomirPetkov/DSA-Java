3421861ba20d54832f5922ece18e11576120d872
dramp <dramppet@abv.bg>
2023-12-01 17:39:55 +0200

    UniqueUsername

--- Set_Map_and_HashTable/SoftUni/Lab/src/UniqueUsername.java
+++ Set_Map_and_HashTable/SoftUni/Lab/src/UniqueUsername.java
@@ -1,5 +1,30 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 public class UniqueUsername {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        Set<String> parkingLot = new HashSet<>();
+
+        String input = sc.nextLine();
+
+        while (!input.equals("END")){
+            String[] token = input.split(", ");
+            String record = token[0];
+            String carNumber = token[1];
+
+            if (record.equals("IN")){
+                parkingLot.add(carNumber);
+            }else {
+                parkingLot.remove(carNumber);
+            }
+            input = sc.nextLine();
+        }
+        if (parkingLot.isEmpty()){
+            System.out.println("Parking Lot is Empty");
+        }
+        parkingLot.stream().forEach(System.out::println);
     }
 }
